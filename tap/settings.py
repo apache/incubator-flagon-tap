56a9bbf1d444772e4811c9c40ea11f346ffd01d5
Alex Ford <aford@draper.com>
2016-06-14 00:08:08 +0000

    adding email user authentication

--- tap/settings.py
+++ tap/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'custom_user',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -51,6 +52,8 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'tap.urls'
+
+AUTH_USER_MODEL = 'custom_user.EmailUser'
 
 TEMPLATES = [
     {
