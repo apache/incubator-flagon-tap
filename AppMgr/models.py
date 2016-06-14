493ea078ef0a9691f934f5e66563720e92f04792
Alex Ford <aford@draper.com>
2016-06-14 00:27:43 +0000

    adding stub classes

--- AppMgr/models.py
+++ AppMgr/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+
+    # additional user parameters
+
+    def __unicode__(self):
+        return self.user.email
+
+class Organization(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
+
+class Application(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
