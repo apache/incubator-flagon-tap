92bc68f44d6aa65927a4b12eb4074229386ba61f
Alex Ford <aford@draper.com>
2016-06-14 00:10:27 +0000

    adding django application for managing applications

--- tap/settings.py
+++ tap/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'custom_user',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'custom_user',
+    'AppMgr',
 )
 
 MIDDLEWARE_CLASSES = (
