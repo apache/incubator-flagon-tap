86aa309634b86b5c2ec453e9cfa98fdfdea78dc6
Alex Ford <aford@draper.com>
2016-06-15 22:03:52 +0000

    changed settings location

--- tap/wsgi.py
+++ tap/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+sys.path.append('./tap/settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tap.production")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tap.settings")
-
 application = get_wsgi_application()
