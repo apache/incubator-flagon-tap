b76a99d04f48044b36581d9303bc8b0a327d212c
Arthi Vezhavendan <arthivez@gmail.com>
2017-09-15 04:10:17 -0400

    Added config file for senssoft related urls

---
+++ secrets/senssoftconfig.js
@@ -0,0 +1,5 @@
+var distill_url = { "distill_url" : 'http://vlsmsbx.draper.com:8090/sankey/userale-js'};
+
+ export default distill_url;
+
+//DISTILL_URL = 'http://vlsmsbx.draper.com:8090/sankey/userale-js'
