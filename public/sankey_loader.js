e618bd2711887ebe1669e61b27b2d0465d48b766
Arthi Vezhavendan <arthivez@gmail.com>
2017-09-18 11:51:39 -0400

    Modified urls to work with Distill docker image

--- public/sankey_loader.js
+++ public/sankey_loader.js
@@ -1,29 +1,30 @@
+// var sankey_blankexample = 
+// {"nodes": 
+// 	[{
+//       "name": "Loading"
+//     }, 
+//     {
+//       "name": "wait..."
+//     }, 
+// 	],
+// "links": 
+// 	[{
+//       "duration": 0, 
+//       "pathLength": 1, 
+//       "sequenceID": "Loading...", 
+//       "source": 0, 
+//       "sourceName": "Loading...", 
+//       "target": 1, 
+//       "targetChange": 1, 
+//       "targetName": "Loading...", 
+//       "type": "load", 
+//       "typeChange": 1, 
+//       "value": 7
+//     }, ]};
+
 var sankey_blankexample = 
 {"nodes": 
-	[{
-      "name": "Loading"
-    }, 
-    {
-      "name": "wait..."
-    }, 
-	],
+  [],
 "links": 
-	[{
-      "duration": 0, 
-      "pathLength": 1, 
-      "sequenceID": "Loading...", 
-      "source": 0, 
-      "sourceName": "Loading...", 
-      "target": 1, 
-      "targetChange": 1, 
-      "targetName": "Loading...", 
-      "type": "load", 
-      "typeChange": 1, 
-      "value": 7
-    }, ]};
+  []};
  export default sankey_blankexample;
-
-// var histogram_example = [{"counts": [188, 14], "target": "div.ag-cell ag-cell-no-focus cell-col-1 ag-cell-value", "types": ["mouseover", "wheel"], "pathLength": 25}, {"counts": [160, 17], "target": "div.ag-cell ag-cell-no-focus cell-col-0 ag-cell-value", "types": ["mouseover", "wheel"], "pathLength": 25}, {"counts": [94, 2], "target": "div.ag-cell ag-cell-no-focus cell-col-2 ag-cell-value", "types": ["mouseover", "wheel"], "pathLength": 25}, {"counts": [74, 10, 2], "target": "#document", "types": ["scroll", "mouseover", "load"], "pathLength": 2}, {"counts": [74, 10, 2], "target": "div.ag-header-cell-label", "types": ["mouseover", "click", "wheel"], "pathLength": 24}, {"counts": [70, 16], "target": "rect.[object SVGAnimatedString]", "types": ["mouseover", "wheel"], "pathLength": 19}, {"counts": [45, 8, 1], "target": "div#gridster-div.ng-scope gridster gridster-desktop gridster-loaded", "types": ["mouseover", "wheel", "click"], "pathLength": 6}, {"counts": [43, 9], "target": "svg#plot.[object SVGAnimatedString]", "types": ["mouseover", "wheel"], "pathLength": 15}, {"counts": [42, 5], "target": "div.form-group ng-scope", "types": ["mouseover", "wheel"], "pathLength": 19}, {"counts": [31, 15, 1], "target": "span.ag-header-cell-text", "types": ["mouseover", "click", "dblclick"], "pathLength": 25}, {"counts": [43, 2, 1], "target": "div.ng-scope superclass ng-isolate-scope", "types": ["mouseover", "click", "wheel"], "pathLength": 10}, 
-//{"counts": [28, 15], "target": "div.ag-body-viewport", "types": ["scroll", "mouseover"], "pathLength": 22}, 
-//{"counts": [38, 3], "target": "span", "types": ["mouseover", "click"], "pathLength": 26}, {"counts": [39], "target": "div.ag-list-item ag-not-dragging", "types": ["mouseover"], "pathLength": 23}, {"counts": [20, 18], "target": "div.ng-scope ng-isolate-scope filter-directive", "types": ["mouseover", "wheel"], "pathLength": 10}, {"counts": [32, 1], "target": "div", "types": ["mouseover", "wheel"], "pathLength": 18}, {"counts": [31, 1], "target": "div.text-container headers-container", "types": ["mouseover", "click"], "pathLength": 13}, {"counts": [31], "target": "div.visualization dataTableDirective", "types": ["mouseover"], "pathLength": 12}, {"counts": [30], "target": "div.visualization-drag-handle", "types": ["mouseover"], "pathLength": 10}, {"counts": [24, 4], "target": "div.popover-content", "types": ["mouseover", "scroll"], "pathLength": 17}];
-//  export default histogram_example;
--- secrets/senssoftconfig.js
+++ secrets/senssoftconfig.js
@@ -1,4 +1,7 @@
-var distill_url = { "distill_url" : 'http://vlsmsbx.draper.com:8090/sankey/userale-js'};
+//var distill_url = { "distill_url" : 'http://vlsmsbx.draper.com:8090/sankey/userale-js'};
+var distill_url = { "distill_url" : 'http://localhost:8090/sankey/userale'};
+//var distill_url = { "distill_url" : 'http://distill:8090/sankey/userale'};
+
 
  export default distill_url;
 
